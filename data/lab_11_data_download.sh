6f6f42f44cddd56697b27a07e8c42bf3ba4454da
jq <joshua.quan@berkeley.edu>
2023-04-27 10:21:14 -0700

    Update lab_11_data_download.sh

--- data/lab_11_data_download.sh
+++ data/lab_11_data_download.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zkueAELQui4aXZUMKLp1fulO2cVP6HCn' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zkueAELQui4aXZUMKLp1fulO2cVP6HCn" -O censoc_bfdw.csv.gz && rm -rf /tmp/cookies.txt
-
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JtuR21hQef1MeiIWYCiolX_tMTAjWvP4' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JtuR21hQef1MeiIWYCiolX_tMTAjWvP4" -O censoc_175_2023.csv && rm -rf /tmp/cookies.txt
